f04d86b9e94717aa79e914496d98519ebbaa6b46
Gabriel Kobyz <gabrielkobyz@gmail.com>
2019-05-09 20:57:52 +0700

    added translations

---
+++ scripts/views/translations.js
@@ -0,0 +1,64 @@
+export const LANG                  = {en: 'En', ru: 'Ру'}
+
+export const ACCOUNTS              = {en: 'Accounts', ru: 'Счета'}
+export const CATEGORIES            = {en: 'Categories', ru: 'Категории'}
+export const TRANSACTIONS          = {en: 'Transactions', ru: 'Транзакции'}
+export const PAYEES                = {en: 'Payees', ru: 'Плательщики'}
+
+export const DATE                  = {en: 'Date', ru: 'Дата'}
+export const TYPE                  = {en: 'Type', ru: 'Тип'}
+export const TITLE                 = {en: 'Title', ru: 'Название'}
+export const AMOUNT                = {en: 'Amount', ru: 'Сумма'}
+export const ACCOUNT               = {en: 'Account', ru: 'Счет'}
+export const PAYEE                 = {en: 'Payee', ru: 'Плательщик'}
+export const CATEGORY              = {en: 'Category', ru: 'Категория'}
+export const COMMENT               = {en: 'Comment', ru: 'Комментарий'}
+
+export const OUTCOME               = {en: 'Outcome', ru: 'Расход'}
+export const INCOME                = {en: 'Income', ru: 'Доход'}
+export const TRANSFER              = {en: 'Transfer', ru: 'Перевод'}
+export const I_LOANED              = {en: 'I loaned', ru: 'Я дал в долг'}
+export const I_BORROWED            = {en: 'I borrowed', ru: 'Я взял в долг'}
+
+export const NEW_ACCOUNT           = {en: 'New account', ru: 'Добавление счета'}
+export const NEW_CATEGORY          = {en: 'New category', ru: 'Добавление категории'}
+export const NEW_PAYEE             = {en: 'New payee', ru: 'Добавление плательщика'}
+export const NEW_TRANSACTION       = {en: 'New transaction', ru: 'Добавление транзакции'}
+
+export const EDIT_ACCOUNT          = {en: 'Edit account', ru: 'Редактирование счета'}
+export const EDIT_CATEGORY         = {en: 'Edit category', ru: 'Редактирование категории'}
+export const EDIT_PAYEE            = {en: 'Edit payee', ru: 'Редактирование плательщика'}
+export const EDIT_TRANSACTION      = {en: 'Edit transaction', ru: 'Редактирование транзакции'}
+
+export const DELETE_ACCOUNT        = {en: 'Delete account?', ru: 'Удалить счет?'}
+export const DELETE_CATEGORY       = {en: 'Delete category?', ru: 'Удалить категорию?'}
+export const DELETE_PAYEE          = {en: 'Delete payee?', ru: 'Удалить плательщика?'}
+export const DELETE_TRANSACTION    = {en: 'Delete transaction?', ru: 'Удалить транзакцию?'}
+
+export const OK                    = {en: 'Ok', ru: 'Ок'}
+export const CANCEL                = {en: 'Cancel', ru: 'Отмена'}
+export const DELETE                = {en: 'Delete', ru: 'Удалить'}
+export const SUBMIT                = {en: 'Submit', ru: 'Подтвердить'}
+
+export const LOGOUT                = {en: 'Logout', ru: 'Выход'}
+
+export const FETCHING_USER         = {en: 'Fetching user', ru: 'Загрузка пользователя...'}
+export const FETCHING_ACCOUNTS     = {en: 'Fetching accounts', ru: 'Загрузка счетов...'}
+export const FETCHING_CATEGORIES   = {en: 'Fetching categories', ru: 'Загрузка категорий...'}
+export const FETCHING_PAYEES       = {en: 'Fetching payees', ru: 'Загрузка плательщиков...'}
+export const FETCHING_TRANSACTIONS = {en: 'Fetching transactions', ru: 'Загрузка транзакций...'}
+
+export const ACCOUNT_CREATED       = {en: 'Account created', ru: 'Счет добавлен'}
+export const CATEGORY_CREATED      = {en: 'Category created', ru: 'Категория добавлена'}
+export const PAYEE_CREATED         = {en: 'Payee created', ru: 'Плательщик добавлен'}
+export const TRANSACTION_CREATED   = {en: 'Transaction created', ru: 'Транзакция добавлена'}
+
+export const ACCOUNT_UPDATED       = {en: 'Account updated', ru: 'Счет обновлен'}
+export const CATEGORY_UPDATED      = {en: 'Category updated', ru: 'Категория обновлена'}
+export const PAYEE_UPDATED         = {en: 'Payee updated', ru: 'Плательщик обновлен'}
+export const TRANSACTION_UPDATED   = {en: 'Transaction updated', ru: 'Транзакция обновлена'}
+
+export const ACCOUNT_DELETED       = {en: 'Account deleted', ru: 'Счет удален'}
+export const CATEGORY_DELETED      = {en: 'Category deleted', ru: 'Категория удалена'}
+export const PAYEE_DELETED         = {en: 'Payee deleted', ru: 'Плательщик удален'}
+export const TRANSACTION_DELETED   = {en: 'Transaction deleted', ru: 'Транзакция удалена'}
