bbede6b56db05f115744b6259d4f048edbb745bc
Gabriel Kobyz <gabrielkobyz@gmail.com>
2020-05-19 16:43:46 +0700

    wip: migration to ts: removed unnecessary constructor in ViewComponent

--- src/scripts/views/misc.tsx
+++ src/scripts/views/misc.tsx
@@ -11,11 +11,6 @@ import * as e from '../env'
 export class ViewComponent<P = any, S = any> extends React.Component<P, S> {
   static contextType: React.Context<t.AppContext> = e.Context
   declare context: React.ContextType<typeof e.Context>
-
-  constructor(props: Readonly<P>, context?: t.AppContext) {
-    super(props, context)
-    // this.render = renderWithArg
-  }
 }
 
 // function renderWithArg() {
