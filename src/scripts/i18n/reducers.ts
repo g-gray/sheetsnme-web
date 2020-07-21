eb8eb468cc130c6e8093a3728b70baa64b60604c
Gabriel Kobyz <gabrielkobyz@gmail.com>
2020-07-21 12:58:21 +0700

    fixed initialization of default i18n state
    * initialize i18n reducer after lang constants initialization

--- src/scripts/i18n/reducers.ts
+++ src/scripts/i18n/reducers.ts
@@ -7,32 +7,6 @@ import * as u from './../utils'
 
 import * as a from './actions'
 
-export const defaultState: t.i18nState = {
-  lang: defLang(),
-}
-
-export const i18n = (state = defaultState, action: a.I18nActions) => {
-  switch (action.type) {
-    case a.NEXT_LANG: {
-
-      const {lang} = state
-      const nextLang = selectNextLang(lang)
-
-      u.storageWrite(['lang'], nextLang)
-
-      return {
-        ...state,
-        lang: nextLang,
-      }
-    }
-
-    default:
-      return state
-  }
-}
-
-
-
 export function defLang(): t.LANG {
   return QUERY_LANG || STORAGE_LANG || DEFAULT_LANG
 }
@@ -56,6 +30,31 @@ export const DEFAULT_LANG: t.LANG = fpx.intersection(
 
 function langPrefix(langCode: string): string {
   return langCode.split('-')[0]
+}
+
+export const defaultState: t.i18nState = {
+  lang: defLang(),
+}
+
+export const i18n = (state = defaultState, action: a.I18nActions) => {
+  switch (action.type) {
+    case a.NEXT_LANG: {
+
+      const {lang} = state
+
+      const nextLang = selectNextLang(lang)
+
+      u.storageWrite(['lang'], nextLang)
+
+      return {
+        ...state,
+        lang: nextLang,
+      }
+    }
+
+    default:
+      return state
+  }
 }
 
 
