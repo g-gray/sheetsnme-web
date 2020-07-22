f2e0db9ddbbde836b0a25a38b30ca64fcc104089
Gabriel Kobyz <gabrielkobyz@gmail.com>
2020-07-22 17:21:47 +0700

    corrected dashboard layout

--- src/scripts/dashboard/views.tsx
+++ src/scripts/dashboard/views.tsx
@@ -1,5 +1,3 @@
-import * as t from '../types'
-
 import React from 'react'
 
 import * as v from '../views'
@@ -12,12 +10,41 @@ type DashboardPageProps = {}
 
 export class DashboardPage extends v.ViewComponent<DashboardPageProps> {
   render() {
+    const {context: {isMobile}} = this
+
+    if (isMobile) {
+      return (
+        <v.DashboardPageLayout>
+          <div className='col-start-stretch gaps-v-1 padding-h-0x5'>
+            <div className='bg-surface shadow-dept-1'>
+              <BalancesChart />
+            </div>
+            <div className='bg-surface shadow-dept-1'>
+              <DebtsChart />
+            </div>
+            <div className='bg-surface shadow-dept-1'>
+              <SpendingsChart />
+            </div>
+          </div>
+        </v.DashboardPageLayout>
+      )
+    }
 
     return (
       <v.DashboardPageLayout>
-        <BalancesChart />
-        <SpendingsChart />
-        <DebtsChart />
+        <div className='col-start-stretch gaps-v-1'>
+          <div className='row-between-start gaps-h-1'>
+            <div className='flex-1 bg-surface shadow-dept-1'>
+              <BalancesChart />
+            </div>
+            <div className='flex-1 bg-surface shadow-dept-1'>
+              <DebtsChart />
+            </div>
+          </div>
+          <div className='flex-1 bg-surface shadow-dept-1'>
+            <SpendingsChart />
+          </div>
+        </div>
       </v.DashboardPageLayout>
     )
   }
